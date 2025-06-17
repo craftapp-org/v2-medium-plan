58a4062796a7ede1075e19a83c6226094fe03661
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-06-17 23:22:39 +0430

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -98,11 +98,11 @@ async def get_data():
 async def debug_env():
     return {
         "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
-        "AWS_SECRET_ACCESS_KEY": bool(os.getenv("AWS_SECRET_ACCESS_KEY")),  # don't leak actual key
+        "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),  # don't leak actual key
         "AWS_REGION": os.getenv("AWS_REGION"),
         "S3_BUCKET_NAME": os.getenv("S3_BUCKET_NAME"),
         "DB_USER": os.getenv("DB_USER"),
-        "DB_PASSWORD": bool(os.getenv("DB_PASSWORD")),  # hide actual value
+        "DB_PASSWORD": os.getenv("DB_PASSWORD"),  # hide actual value
         "DB_NAME": os.getenv("DB_NAME"),
         "DB_HOST": os.getenv("DB_HOST"),
         "DB_PORT": os.getenv("DB_PORT"),
